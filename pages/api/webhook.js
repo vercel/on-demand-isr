dea5392a7981f7cc89b8328472c119db6b75cc5f
Lee Robinson <lrobinson2011@gmail.com>
2022-02-20 11:02:27 -0600

    Revalidate index on comment change

--- pages/api/webhook.js
+++ pages/api/webhook.js
@@ -1,11 +1,11 @@
-import { createHmac } from "crypto";
+import { createHmac } from 'crypto';
 
 export default async function handleWebhook(req, res) {
   // verify the webhook signature request against the
   // unmodified, unparsed body
   const body = await getRawBody(req);
   if (!body) {
-    res.status(400).send("Bad request (no body)");
+    res.status(400).send('Bad request (no body)');
     return;
   }
 
@@ -13,47 +13,42 @@ export default async function handleWebhook(req, res) {
 
   // compute our signature from the raw body
   const secret = process.env.GITHUB_WEBHOOK_SECRET;
-  const signature = req.headers["x-hub-signature-256"];
+  const signature = req.headers['x-hub-signature-256'];
   const computedSignature =
-    "sha256=" + createHmac("sha256", secret).update(body).digest("hex");
+    'sha256=' + createHmac('sha256', secret).update(body).digest('hex');
 
   if (computedSignature === signature) {
     console.log(
-      "event",
-      req.headers["x-github-event"],
-      "action",
+      'event',
+      req.headers['x-github-event'],
+      'action',
       jsonBody.action,
-      "issue",
+      'issue',
       jsonBody.issue?.title,
       jsonBody.issue?.number
     );
 
-    const eventType = req.headers["x-github-event"];
     const issueNumber = jsonBody.issue?.number;
 
-    if (eventType === "issues") {
-      // issue opened or edited
-      await res.unstable_revalidate("/");
-      await res.unstable_revalidate(`/${issueNumber}`);
-    } else {
-      // comment created or edited
-      await res.unstable_revalidate(`/${issueNumber}`);
-    }
+    // issue opened or edited
+    // comment created or edited
+    await res.unstable_revalidate('/');
+    await res.unstable_revalidate(`/${issueNumber}`);
 
-    return res.status(200).send("Success!");
+    return res.status(200).send('Success!');
   } else {
-    return res.status(403).send("Forbidden");
+    return res.status(403).send('Forbidden');
   }
 }
 
 function getRawBody(req) {
   return new Promise((resolve, reject) => {
     let bodyChunks = [];
-    req.on("end", () => {
-      const rawBody = Buffer.concat(bodyChunks).toString("utf8");
+    req.on('end', () => {
+      const rawBody = Buffer.concat(bodyChunks).toString('utf8');
       resolve(rawBody);
     });
-    req.on("data", (chunk) => bodyChunks.push(chunk));
+    req.on('data', (chunk) => bodyChunks.push(chunk));
   });
 }
 
