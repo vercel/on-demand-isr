0fcd86e78b82cbdb6336f11f5127aa21998548e9
Lee Robinson <lrobinson2011@gmail.com>
2024-05-18 12:02:48 -0500

    Use PPR

--- app/time-ago.tsx
+++ app/time-ago.tsx
@@ -1,11 +1,11 @@
-'use client';
-
 import TimeAgo from 'javascript-time-ago';
 import en from 'javascript-time-ago/locale/en.json';
+import { unstable_noStore } from 'next/cache';
 
 TimeAgo.addDefaultLocale(en);
 const timeAgo = new TimeAgo('en-US');
 
 export function Time({ time }) {
+  unstable_noStore();
   return timeAgo.format(new Date(time));
 }
